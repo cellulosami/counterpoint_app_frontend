1cc6e5a437068af4f00ce51d775ffecb0b6f9748
austin shields <ajshields196@gmail.com>
2021-04-08 18:09:14 -0400

    add ability to select notes length in scoresCreate

--- src/views/Scores.vue
+++ src/views/Scores.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <h1>{{ message }}</h1>
     <p>{{ currentScore }}</p>
+    <button v-on:click="lengthSelect(8)">length 8</button>
+    <button v-on:click="lengthSelect(10)">length 10</button>
     <button v-on:click="scoresCreate">Create!</button>
   </div>
 </template>
@@ -14,7 +16,8 @@ export default {
   data: function () {
     return {
       message: "Scores",
-      currentScore: {}
+      currentScore: {},
+      inputLength: 8
     }
   },
   mounted: function () {
@@ -23,12 +26,18 @@ export default {
   methods: {
     scoresCreate: function () {
       console.log("creating...");
+      let params = {
+        length: this.inputLength
+      }
       axios
-        .post("http://localhost:3000/api/scores")
+        .post("http://localhost:3000/api/scores", params)
         .then(response => {
           console.log(response.data);
           this.currentScore = response.data;
         })
+    },
+    lengthSelect: function (number) {
+      this.inputLength = number;
     }
   }
 }

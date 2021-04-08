9c6b48fd2e5e62e604bd810f1ef4c211d71e7862
austin shields <ajshields196@gmail.com>
2021-04-08 17:51:33 -0400

    initial page setup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Scores</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
@@ -30,3 +30,4 @@
   color: #42b983;
 }
 </style>
+
---
+++ src/views/Scores.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+
+  data: function () {
+    return {
+      message: "Scores"
+    }
+  },
+  created: function () {},
+  methods: {}
+}
+</script>

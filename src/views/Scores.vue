9d090aa96156e9af4bd1edddfe66af4e3a4373da
austin shields <ajshields196@gmail.com>
2021-04-08 17:59:14 -0400

    add scoresCreate action

--- src/views/Scores.vue
+++ src/views/Scores.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <p>{{ currentScore }}</p>
+    <button v-on:click="scoresCreate">Create!</button>
   </div>
 </template>
 
@@ -11,10 +13,23 @@ export default {
 
   data: function () {
     return {
-      message: "Scores"
+      message: "Scores",
+      currentScore: {}
     }
   },
-  created: function () {},
-  methods: {}
+  mounted: function () {
+
+  },
+  methods: {
+    scoresCreate: function () {
+      console.log("creating...");
+      axios
+        .post("http://localhost:3000/api/scores")
+        .then(response => {
+          console.log(response.data);
+          this.currentScore = response.data;
+        })
+    }
+  }
 }
 </script>

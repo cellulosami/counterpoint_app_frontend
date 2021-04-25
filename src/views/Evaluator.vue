18cca739560efdb44f87770b18264cb46f3e22e9
austin shields <ajshields196@gmail.com>
2021-04-25 11:56:01 -0400

    add length selection to evaluator

--- src/views/Evaluator.vue
+++ src/views/Evaluator.vue
@@ -2,8 +2,39 @@
   <div>
     <h1>
       Evaluator
-      {{ notes }}
+      {{ lengthTranslator }}
     </h1>
+    <select class="score-select dropdown" v-model="length">
+      <option>
+        8
+      </option>
+      <option>
+        9
+      </option>
+      <option>
+        10
+      </option>
+      <option>
+        11
+      </option>
+      <option>
+        12
+      </option>
+      <option>
+        13
+      </option>
+      <option>
+        14
+      </option>
+      <option>
+        15
+      </option>
+      <option>
+        16
+      </option>
+    </select>
+    <div>
+    </div>
     <button v-on:click="createEvaluation">
       Evaluate
     </button>
@@ -17,8 +48,20 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      notes: [0, 2, 4, 5, 7, 5, 2, 0]
+      notes: [0, 2, 4, 5, 7, 5, 12, 0],
+      length: 8, 
     }
+  },
+  computed: {
+    lengthTranslator () {
+      let array = [];
+      let i = 0;
+      while (i < this.length) {
+        array.push(0)
+        i++;
+      }
+      return array;
+    },
   },
   methods: {
     createEvaluation: function () {
